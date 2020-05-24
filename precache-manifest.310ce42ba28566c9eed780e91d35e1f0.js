88e2134a776705218d036e8c1f9cd18c280ae61d
Tatiana Churkina <tatiana.churkina@mera.com>
2020-05-24 14:51:05 +0300

    Updates

---
+++ precache-manifest.310ce42ba28566c9eed780e91d35e1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504fa61fd557fc7b5136b5b624cd406c",
+    "url": "/client-side-form-updated/index.html"
+  },
+  {
+    "revision": "98c2f2543714b3004ba8",
+    "url": "/client-side-form-updated/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "9e8bd3c11b66979df6a2",
+    "url": "/client-side-form-updated/static/css/main.f2fa04c8.chunk.css"
+  },
+  {
+    "revision": "98c2f2543714b3004ba8",
+    "url": "/client-side-form-updated/static/js/2.468e6f41.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/client-side-form-updated/static/js/2.468e6f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8bd3c11b66979df6a2",
+    "url": "/client-side-form-updated/static/js/main.fb050ddd.chunk.js"
+  },
+  {
+    "revision": "853042ac9fc6d05475de",
+    "url": "/client-side-form-updated/static/js/runtime-main.d0683d03.js"
+  }
+]);
